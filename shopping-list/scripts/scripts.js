1f73ab9b119a42dc087208f19d9181e385a313f1
Dustin Myers <dustinblaine.myers@gmail.com>
2020-02-06 10:55:14 -0500

    just messing around

--- shopping-list/scripts/scripts.js
+++ shopping-list/scripts/scripts.js
@@ -8,10 +8,23 @@ console.log(inputs);
 
 generateShoppingList.addEventListener('click', function (e) {
     e.preventDefault();
-
+    const newDiv = document.createElement("ul"); 
     console.log("you clicked the button");
 
     inputs.forEach(input => {
+
+        function addElement () { 
+            // create a new div element 
+            
+            // and give it some content 
+            const newContent = document.createTextNode("Hi there and greetings!"); 
+            // add the text node to the newly created div
+            newDiv.appendChild(newContent);  
+          
+            // add the newly created element and its content into the DOM 
+            var currentDiv = document.getElementById("div1"); 
+            document.body.insertBefore(newDiv, currentDiv); 
+          }
         console.log(`This inputs value is ${input.value}`)
     })
 
