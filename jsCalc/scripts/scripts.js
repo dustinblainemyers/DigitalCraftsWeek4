052ade865f3fce15232767f035893a78a1e36a46
Dustin Myers <dustinblaine.myers@gmail.com>
2020-02-05 21:39:41 -0500

    starting to experiment with wiring js

---
+++ jsCalc/scripts/scripts.js
@@ -0,0 +1,19 @@
+// const submitMadLib = document.getElementById("submitmadlib");
+
+
+const calculatorButtons = document.querySelectorAll("div > div");
+console.log(calculatorButtons);
+
+
+
+   
+    //  submitMadLib.addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     outputs.forEach(output => {
+        
+    //     const input =  document.getElementById(output.className);
+    //     output.innerHTML = input.value;
+    //     })
+
+
+    //  })
