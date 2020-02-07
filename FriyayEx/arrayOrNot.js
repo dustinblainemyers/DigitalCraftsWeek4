68e473882b7b2976b66ebe16ebb33385fcc02046
Dustin Myers <dustinblaine.myers@gmail.com>
2020-02-07 10:43:03 -0500

    forgot use strict at top of first exercise

--- FriyayEx/arrayOrNot.js
+++ FriyayEx/arrayOrNot.js
@@ -1,6 +1,6 @@
-// 1. Write a JavaScript function to check whether an `input` is an array or not.
 
-1. Write a JavaScript function to check whether an `input` is an array or not.
+'use strict';
+// 1. Write a JavaScript function to check whether an `input` is an array or not.
 
 function arrayOrNot(input) {
 
